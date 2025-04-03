5bc773245266f8a27aaf9aad018cfcc2a086dfd2
Unicorn <suifengzhuxia@qq.com>
2025-04-03 22:38:11 +0800

    重构

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,6 @@
 AUTHOR = 'Uni星人'
 SITENAME = 'Uni星人的博客'
-SITEURL = "" # 本地开发环境配置（空字符串自动使用 http://localhost:8000 作为根路径）
+SITEURL = ""
 
 PATH = "content"
 
@@ -33,27 +33,3 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
-# 主题设置
-#THEME = 'notmyidea'  # 默认主题
-
-# 静态路径
-# STATIC_PATHS = ['images', 'extra/CNAME','downloads']
-# EXTRA_PATH_METADATA = {
-#     'extra/CNAME': {'path': 'CNAME'},
-# }
-
-# 文章URL结构
-# ARTICLE_URL = 'posts/{slug}/'
-# ARTICLE_SAVE_AS = 'posts/{slug}/index.html'
-# PAGE_URL = 'pages/{slug}/'
-# PAGE_SAVE_AS = 'pages/{slug}/index.html'
-
-# 启用Markdown扩展
-MARKDOWN = {
-    'extension_configs': {
-        'markdown.extensions.codehilite': {'css_class': 'highlight'},
-        'markdown.extensions.extra': {},
-        'markdown.extensions.meta': {},
-    },
-    'output_format': 'html5',
-}
--- publishconf.py
+++ publishconf.py
@@ -8,24 +8,14 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://unicornumr1314.github.io"
+SITEURL = "https://unicornumr1314.github.io/"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
-AUTHOR_FEED_RSS = 'feeds/{slug}.rss.xml' 
 
 DELETE_OUTPUT_DIRECTORY = True
-# 设置英文日期格式
-DATE_FORMATS = {
-    'en': '%a, %d %b %Y',
-}
 
-# 设置中文日期格式
-LOCALE = ['zh_CN.utf8', 'zh_CN.UTF-8']
-DEFAULT_DATE_FORMAT = '%Y年%m月%d日'
-
-THEME = "/pelican-themes/pelican-fh5co-marble"
 # Following items are often useful when publishing
 
 # DISQUS_SITENAME = ""
